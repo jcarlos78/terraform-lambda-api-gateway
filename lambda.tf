f90e7120aebc28f1d00968df7fdf1e22d2b16a44
José Menezes <jcarlos78@gmail.com>
2021-08-30 18:00:31 -0300

    node version update

--- lambda.tf
+++ lambda.tf
@@ -18,7 +18,7 @@ resource "aws_lambda_function" "test_lambda" {
   role             = "${aws_iam_role.iam_for_lambda_tf.arn}"
   handler          = "index.handler"
   source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
-  runtime          = "nodejs6.10"
+  runtime          = "nodejs14.x"
 }
 
 resource "aws_iam_role" "iam_for_lambda_tf" {
