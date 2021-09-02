c8455cac137ccd15fcba3d22d5aea3f2188a02a1
José Menezes <jcarlos78@gmail.com>
2021-09-02 12:57:51 -0300

    added api gateway

--- README.md
+++ README.md
@@ -7,4 +7,11 @@ Learn](https://learn.hashicorp.com/tutorials/terraform/lambda-api-gateway?in=ter
 
 #Commands :
 
+//List bucket
 aws s3 ls $(terraform output -raw lambda_bucket_name)
+
+//Invoke remote lambda
+aws lambda invoke --region=us-east-1 --function-name=$(terraform output -raw function_name) response.json
+
+//Invoke remote api endpoint
+curl "$(terraform output -raw base_url)/hello"
--- main.tf
+++ main.tf
@@ -21,6 +21,9 @@ provider "aws" {
   region = var.aws_region
 }
 
+/**
+Create a renadom animal name for the buckets
+*/
 resource "random_pet" "lambda_bucket_name" {
   prefix = "learn-terraform-functions"
   length = 4
@@ -111,4 +114,81 @@ This policy allows lambda function to write to CloudWatch logs.
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+/**
+API Gateway name and protocol
+*/
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "serverless_lambda_gw"
+  protocol_type = "HTTP"
+}
+
+/**
+Stage for the API Gateway with access logging enabled
+*/
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+  
+  name        = "serverless_lambda_stage"
+  auto_deploy = true
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+/**
+Configures API Gateway to use your Lambda function
+*/
+resource "aws_apigatewayv2_integration" "hello_world" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.hello_world.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+/**
+Map HTTP request to a target
+*/
+resource "aws_apigatewayv2_route" "hello_world" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
+}
+
+/**
+Define a log group to store access logs for the aws_apigatewayv2_stage.lambda API Gateway stage
+*/
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 30
+}
+
+/**
+Gives API Gateway permission to invoke the Lambda function
+*/
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
